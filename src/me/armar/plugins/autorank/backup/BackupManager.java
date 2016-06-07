1fff721124228ada5eaafce2d767cdff3c00a5ee
Minecraft User <rogerk@pvp-minecraft.com>
2016-06-07 11:37:20 -0500

    Removed backup system

--- src/me/armar/plugins/autorank/backup/BackupManager.java
+++ src/me/armar/plugins/autorank/backup/BackupManager.java
@@ -21,13 +21,9 @@ import me.armar.plugins.autorank.Autorank;
 public class BackupManager {
 
 	private final Autorank plugin;
-	private final BackupDataManager backupDataManager;
 
 	public BackupManager(final Autorank plugin) {
 		this.plugin = plugin;
-		backupDataManager = new BackupDataManager(plugin);
-
-		backupDataManager.createNewFile();
 	}
 
 	/**
@@ -37,31 +33,6 @@ public class BackupManager {
 	 * @param storePath Path to backup the file to, can be null.
 	 */
 	public void backupFile(final String sourceFileName, final String storePath) {
-		// CAN ONLY COPY YML
-		final String folderPath = plugin.getDataFolder().getAbsolutePath() + File.separator;
-		final File sourceFile = new File(folderPath + sourceFileName);
-
-		File copyFile = null;
-
-		if (storePath == null) {
-			copyFile = new File(
-					folderPath + sourceFileName.replace(".yml", "") + "-backup-" + System.currentTimeMillis() + ".yml");
-		} else {
-			copyFile = new File(storePath.replace(".yml", "") + "-backup-" + System.currentTimeMillis() + ".yml");
-		}
-
-		// Create folder if it doesn't exist.
-		copyFile.getParentFile().mkdirs();
-
-		try {
-			Files.copy(sourceFile, copyFile);
-			plugin.debugMessage("Made backup of '" + sourceFileName + "'!");
-		} catch (final IOException e) {
-			plugin.getServer().getConsoleSender().sendMessage(
-					"[Autorank] " + ChatColor.RED + "Could not find " + sourceFileName + ", so didn't backup.");
-			//e.printStackTrace();
-		}
-
 	}
 
 	/**
@@ -70,39 +41,6 @@ public class BackupManager {
 	 * This will make a backup of each file every day.
 	 */
 	public void startBackupSystem() {
-		// Makes a backup every day
-		plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, new Runnable() {
-			@Override
-			public void run() {
-
-				// Older than a day
-				if ((System.currentTimeMillis() - backupDataManager.getLatestBackup("data")) > 86400000) {
-					plugin.getLogger().info("Making a backup of data.yml.");
-
-					plugin.getBackupManager().backupFile("Data.yml", plugin.getDataFolder().getAbsolutePath()
-							+ File.separator + "backups" + File.separator + "data.yml");
-
-					// Update latest backup time
-					backupDataManager.getConfig().set("data", System.currentTimeMillis());
-				}
-
-				// Older than a day
-				if ((System.currentTimeMillis() - backupDataManager.getLatestBackup("playerdata")) > 86400000) {
-					plugin.getLogger().info("Making a backup of playerdata.yml.");
-
-					// Before running, backup stuff.
-					plugin.getBackupManager().backupFile("/playerdata/playerdata.yml",
-							plugin.getDataFolder().getAbsolutePath() + File.separator + "backups" + File.separator
-									+ "playerdata.yml");
-
-					// Update latest backup time
-					backupDataManager.getConfig().set("playerdata", System.currentTimeMillis());
-				}
-
-				// Save config
-				backupDataManager.saveConfig();
-			}
-		}, 0, 1728000);
 	}
 
 }
